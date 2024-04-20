c1d4cef4d8f375b7d5471e7a8cd42a49febc8d4b
asaunde78 <saundersasher78@gmail.com>
2024-04-19 20:07:46 -0400

    updated dockerfile to install chrome

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ FROM python:3.10.12
 RUN apt-get -y update
 RUN pip install poetry 
 COPY . . 
-RUN cd scraperrework/scraper
+
+RUN chmod +x ./setup.sh
 RUN ./setup.sh
-RUN cd ../..
+
 
 RUN poetry install
 ENTRYPOINT ["poetry"]
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 
-if [ -d "chrome/" ] ; then
+if [ -d "scraperrework/scraper/chrome/" ] ; then
     echo "chrome installed already!"
     exit
 fi
 echo "missing chrome; installing"
 wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5982.0/linux64/chrome-linux64.zip -P chrome
-cd chrome
+cd scraperrework/scraper/chrome
 echo "unzipping"
 unzip chrome-linux64.zip
 rm -f chrome-linux64.zip
